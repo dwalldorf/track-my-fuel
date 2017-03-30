9ad11654c92ac68860cf79ab8a01bf68b095563b
Daniel Walldorf <d.walldorf@me.com>
2017-03-31 00:45:36 +0200

    dummy test

---
+++ src/test/java/com/dwalldorf/fuel/controller/VersionControllerTest.java
@@ -0,0 +1,13 @@
+package com.dwalldorf.fuel.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class VersionControllerTest {
+
+    @Test
+    public void testDummy() {
+        assertTrue(true);
+    }
+}
