09f7ecfe4e11b6b05a20f741152f79b11ad07be3
Daniel Walldorf <d.walldorf@me.com>
2017-03-31 00:42:01 +0200

    wip

---
+++ src/main/java/com/dwalldorf/fuel/config/ApplicationProperties.java
@@ -0,0 +1,32 @@
+package com.dwalldorf.fuel.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Holds values from application.properties prefixed <code>app</code>.
+ */
+@Component
+public class ApplicationProperties {
+
+    @Value("${app.name}")
+    private String name;
+
+    @Value("${app.description}")
+    private String description;
+
+    @Value("${app.version}")
+    private String version;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
