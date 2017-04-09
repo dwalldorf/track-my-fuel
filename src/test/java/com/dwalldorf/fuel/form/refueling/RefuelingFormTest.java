dc18942212058aa22744da9166ec496752629f90
Daniel Walldorf <d.walldorf@me.com>
2017-04-09 12:01:19 +0200

    test stub

---
+++ src/test/java/com/dwalldorf/fuel/form/refueling/RefuelingFormTest.java
@@ -0,0 +1,14 @@
+package com.dwalldorf.fuel.form.refueling;
+
+import static org.junit.Assert.fail;
+
+import com.dwalldorf.fuel.BaseTest;
+import org.junit.Test;
+
+public class RefuelingFormTest extends BaseTest {
+
+    @Test
+    public void test() {
+        fail("write tests");
+    }
+}
