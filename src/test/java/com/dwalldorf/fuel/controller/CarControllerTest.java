96568925531d18a4c79cdd33e6dfe1c5641ce5c2
Daniel Walldorf <d.walldorf@me.com>
2017-04-09 16:35:52 +0200

    introduce CarControllerTest

---
+++ src/test/java/com/dwalldorf/fuel/controller/CarControllerTest.java
@@ -0,0 +1,33 @@
+package com.dwalldorf.fuel.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import com.dwalldorf.fuel.BaseTest;
+import com.dwalldorf.fuel.service.CarService;
+import com.dwalldorf.fuel.service.UserService;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class CarControllerTest extends BaseTest {
+
+    @Mock
+    private CarService mockCarService;
+
+    @Mock
+    private UserService mockUserService;
+
+    private CarController carController;
+
+    @Override
+    protected void setUp() {
+        this.carController = new CarController(mockCarService, mockUserService);
+    }
+
+    @Test
+    public void testIndexAction_ReturnsListPage() {
+        final String expectedViewName = carController.listAction();
+        final String actualViewName = carController.indexAction();
+
+        assertEquals(expectedViewName, actualViewName);
+    }
+}
