f5ac9664229029a4ea82800fa7b8c11677464418
Daniel Walldorf <d.walldorf@me.com>
2017-04-09 14:51:55 +0200

    introduce CarFormTest

---
+++ src/test/java/com/dwalldorf/fuel/form/car/CarFormTest.java
@@ -0,0 +1,14 @@
+package com.dwalldorf.fuel.form.car;
+
+import static org.junit.Assert.fail;
+
+import com.dwalldorf.fuel.BaseTest;
+import org.junit.Test;
+
+public class CarFormTest extends BaseTest {
+
+    @Test
+    public void test() {
+        fail("write tests");
+    }
+}
