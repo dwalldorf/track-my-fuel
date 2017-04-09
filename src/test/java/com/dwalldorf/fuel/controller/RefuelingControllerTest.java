8aa1711c34cca95bcae9c9a19ec884fb1bdb382c
Daniel Walldorf <d.walldorf@me.com>
2017-04-09 13:46:53 +0200

    introduce RefuelingControllerTest

---
+++ src/test/java/com/dwalldorf/fuel/controller/RefuelingControllerTest.java
@@ -0,0 +1,14 @@
+package com.dwalldorf.fuel.controller;
+
+import static org.junit.Assert.fail;
+
+import com.dwalldorf.fuel.BaseTest;
+import org.junit.Test;
+
+public class RefuelingControllerTest extends BaseTest {
+
+    @Test
+    public void test() {
+        fail("write tests");
+    }
+}
